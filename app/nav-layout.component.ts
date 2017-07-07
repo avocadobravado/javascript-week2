88863b5afd932726073cddd9ead48d0ac35ff456
Rose Sponseller <rosesponseller@gmail.com>
2017-07-07 12:34:15 -0700

    Set display flex on the navbar

--- app/nav-layout.component.ts
+++ app/nav-layout.component.ts
@@ -5,9 +5,19 @@ import { AnimalComponent } from './animal-inventory.component'
  @Component({
    selector: 'nav-layout',
    template: `
-        <div>
-        <h1>This is the nav</h1>
-        </div>
+   <nav class="navbar">
+    <div class="container-fluid">
+    <!-- Navbar brand -->
+      <div class="navbar-header">
+      <a class="navbar-brand" href="index.html"><img src="http://gloria-project.eu/wp-content/uploads/2012/06/logo-128-transp.png" alt="Rose City Zoo">
+      </a>
+      </div>
+    <!-- Right side of navbar -->
+      <span class="nav navbar-nav navbar-right">
+      <p><em>Admin Login</em></p>
+      </span>
+    </div><!--Container close -->
+   </nav>
      `
  })
 
