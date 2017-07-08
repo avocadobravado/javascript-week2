520546369fb7694f90dcf2468dd19c0be73ef727
Rose Sponseller <rosesponseller@gmail.com>
2017-07-07 20:28:39 -0700

    Make sure HTML is properly indented

--- app/nav-layout.component.ts
+++ app/nav-layout.component.ts
@@ -5,8 +5,7 @@ import { AnimalComponent } from './animal-inventory.component'
  @Component({
    selector: 'nav-layout',
    template: `
-
-<!--Monkey business-->
+   <!--Monkey business-->
     <div class="monkey-wrapper">
       <div class="monkey">
         <img src="resources/img/monkey.png" class="monkey-img">
@@ -16,8 +15,8 @@ import { AnimalComponent } from './animal-inventory.component'
     <div class="container-fluid">
     <!-- Navbar brand -->
       <div class="navbar-header">
-      <a class="navbar-brand" href="index.html"><img src="resources/img/Logo.png" alt="Rose City Zoo">
-      </a>
+        <a class="navbar-brand" href="index.html"><img src="resources/img/Logo.png" alt="Rose City Zoo">
+        </a>
       </div>
     </div><!--Container close -->
    </nav>
